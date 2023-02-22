d8cc2ad2a84d7aca039148ce958a92d098c14a25
Jackson Kenneth Clayton <124742643+MiamiGameDesign@users.noreply.github.com>
2023-02-22 15:41:55 -0500

    asdf

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    Rigidbody mainBody;
+    private void Start()
+    {
+        mainBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        mainBody.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+}
