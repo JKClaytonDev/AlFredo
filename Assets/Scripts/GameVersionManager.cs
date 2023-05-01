5b28c015ce2585699fd2dfbd59268e6fe7d8b10a
Jackson Kenneth Clayton <59662161+JKClaytonDev@users.noreply.github.com>
2023-05-01 11:38:15 -0400

    more code changes, adding comments

--- Assets/Scripts/GameVersionManager.cs
+++ Assets/Scripts/GameVersionManager.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameVersionManager : MonoBehaviour
 {
-    public bool Android;
-    public bool p1LeftTap;
-    public bool p2LeftTap;
-    public bool p1RightTap;
-    public bool p2RightTap;
-    public bool p1UpTap;
-    public bool p2UpTap;
-    public bool p1DownTap;
-    public bool p2DownTap;
-    public bool p1ShootTap;
-    public bool p2ShootTap;
+    public bool Android, p1LeftTap, p2LeftTap, p1RightTap, p2RightTap, p1UpTap, p2UpTap, p1DownTap, p2DownTap, p1ShootTap, p2ShootTap;
     public GameObject mobileUI;
     public Text p1DeadText;
     private void Start()
@@ -26,54 +16,84 @@ public class GameVersionManager : MonoBehaviour
     }
     private void Update()
     {
-        if (Android)
+        // Check if the platform is Android
+        switch (Android)
         {
-            mobileUI.SetActive(true);
-            return;
-        }
-        mobileUI.SetActive(false);
-        p1LeftTap = Input.GetKeyDown(KeyCode.A);
-        p1RightTap = Input.GetKeyDown(KeyCode.D);
-        p1UpTap = Input.GetKeyDown(KeyCode.W);
-        p1DownTap = Input.GetKeyDown(KeyCode.S);
-        p1ShootTap = Input.GetKeyDown(KeyCode.Q);
+            // If it is, enable the mobile UI and exit the method
+            case true:
+                mobileUI.SetActive(true);
+                return;
+            // If it's not Android, disable the mobile UI and check for keyboard input
+            default:
+                mobileUI.SetActive(false);
+                p1LeftTap = Input.GetKeyDown(KeyCode.A);
+                p1RightTap = Input.GetKeyDown(KeyCode.D);
+                p1UpTap = Input.GetKeyDown(KeyCode.W);
+                p1DownTap = Input.GetKeyDown(KeyCode.S);
+                p1ShootTap = Input.GetKeyDown(KeyCode.Q);
 
-        p2LeftTap = Input.GetKeyDown(KeyCode.LeftArrow);
-        p2RightTap = Input.GetKeyDown(KeyCode.RightArrow);
-        p2UpTap = Input.GetKeyDown(KeyCode.UpArrow);
-        p2DownTap = Input.GetKeyDown(KeyCode.DownArrow);
-        p2ShootTap = Input.GetKeyDown(KeyCode.Period);
+                p2LeftTap = Input.GetKeyDown(KeyCode.LeftArrow);
+                p2RightTap = Input.GetKeyDown(KeyCode.RightArrow);
+                p2UpTap = Input.GetKeyDown(KeyCode.UpArrow);
+                p2DownTap = Input.GetKeyDown(KeyCode.DownArrow);
+                p2ShootTap = Input.GetKeyDown(KeyCode.Period);
+                break;
+        }
     }
+
     public void setP1Input(int index)
     {
+        // Reset all the boolean variables for player 1
         resetP1Bool();
-        if (index == 1)
-            p1RightTap = true;
-        if (index == 2)
-            p1LeftTap = true;
-        if (index == 3)
-            p1UpTap = true;
-        if (index == 4)
-            p1DownTap = true;
-        if (index == 5)
-            p1ShootTap = true;
+
+        // Check the value of the index and set the corresponding boolean variable to true
+        switch (index)
+        {
+            case 1:
+                p1RightTap = true;
+                break;
+            case 2:
+                p1LeftTap = true;
+                break;
+            case 3:
+                p1UpTap = true;
+                break;
+            case 4:
+                p1DownTap = true;
+                break;
+            case 5:
+                p1ShootTap = true;
+                break;
+        }
     }
     public void setP2Input(int index)
     {
+        // Reset all the boolean variables for player 2
         resetP2Bool();
-        if (index == 1)
-            p2RightTap = true;
-        if (index == 2)
-            p2LeftTap = true;
-        if (index == 3)
-            p2UpTap = true;
-        if (index == 4)
-            p2DownTap = true;
-        if (index == 5)
-            p2ShootTap = true;
+
+        // Check the value of the index and set the corresponding boolean variable to true
+        switch (index)
+        {
+            case 1:
+                p2RightTap = true;
+                break;
+            case 2:
+                p2LeftTap = true;
+                break;
+            case 3:
+                p2UpTap = true;
+                break;
+            case 4:
+                p2DownTap = true;
+                break;
+            case 5:
+                p2ShootTap = true;
+                break;
+        }
     }
     public void resetP1Bool()
     {
+        // Reset all the boolean variables for player 1
         p1LeftTap = false;
         p1RightTap = false;
         p1UpTap = false;
@@ -83,6 +103,7 @@ public class GameVersionManager : MonoBehaviour
 
     public void resetP2Bool()
     {
+        // Reset all the boolean variables for player 2
         p2LeftTap = false;
         p2RightTap = false;
         p2UpTap = false;
--- Assets/Scripts/playerStatusManager.cs
+++ Assets/Scripts/playerStatusManager.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 public class playerStatusManager : MonoBehaviour
 {
-    public Image p1Sprite;
-    public Image p2Sprite;
-    public Sprite p1WinSprite;
-    public Sprite p2WinSprite;
-    public Sprite p1LoseSprite;
-    public Sprite p2LoseSprite;
-    public Sprite p1TieSprite;
-    public Sprite p2TieSprite;
-    public Sprite p1DeadSprite;
-    public Sprite p2DeadSprite;
     public pelletParticles p;
-    public playerMovement p1;
-    public playerMovement p2;
-    public Animator playerAnim;
-    public Animator player2Anim;
+    public playerMovement p1, p2;
+    public Image p1Sprite, p2Sprite;
+    public Animator playerAnim, player2Anim;
+    public Sprite p1WinSprite, p2WinSprite, p1LoseSprite, p2LoseSprite, p1TieSprite, p2TieSprite, p1DeadSprite, p2DeadSprite;
     // Start is called before the first frame update
 
     public void PlayAnimation(string animName, int index)
@@ -33,11 +23,10 @@ public class playerStatusManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Color pepperColor = Color.white;
-        float subVar = Mathf.Abs(Mathf.Sin(Time.realtimeSinceStartup * 5))/2;
-        pepperColor.g -= subVar+0.3f;
-        pepperColor.b -= subVar + 0.3f;
-        pepperColor.a = 1;
+        float subVar = Mathf.Abs(Mathf.Sin(Time.realtimeSinceStartup * 5)) / 2;
+        Color pepperColor = new Color(1, 0.7f - subVar, 0.7f - subVar);
+        
+
         if (Time.realtimeSinceStartup < p1.pepperTime)
             p1Sprite.color = (Color.white + pepperColor) * 0.5f;
         else
@@ -47,19 +36,10 @@ public class playerStatusManager : MonoBehaviour
         else
             p2Sprite.color = Color.white;
 
-        pepperColor = Color.white;
-        subVar = Mathf.Abs(Mathf.Sin(Time.realtimeSinceStartup * 5)) / 2;
-        pepperColor.g -= subVar + 0.3f;
-        pepperColor.r -= subVar + 0.3f;
-        pepperColor.a = 1;
-        if (p1.onion)
-            p1Sprite.color *= (Color.white + pepperColor) * 0.5f;
-        else
-            p1Sprite.color *= Color.white;
-        if (p2.onion)
-            p2Sprite.color *= (Color.white + pepperColor) * 0.5f;
-        else
-            p2Sprite.color *= Color.white;
+        
+        pepperColor = new Color(1, 1, pepperColor.r);
+        p1Sprite.color *= (p1.onion) ? ((Color.white + pepperColor) * 0.5f) : Color.white;
+        p2Sprite.color *= (p2.onion) ? ((Color.white + pepperColor) * 0.5f) : Color.white;
 
         if (p.score2+10 < p.score1)
         {
